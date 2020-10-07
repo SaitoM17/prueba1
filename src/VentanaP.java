1c4466dcdc769189c9a629a386dcdbf227984e8c
Said Mariano <Said Mariano@Said>
2020-10-07 18:08:09 -0500

    1.1

---
+++ src/VentanaP.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Said Mariano
+ */
+import javax.swing.*;
+
+public class VentanaP {
+        public static void main(String [] args ){
+        JFrame f = new JFrame("Mi ventana :v");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
